2273afd4430e786e6929024fa61e61d594105589
Francisco Javier Luna Toledano <franciscojavier.lunatoledano@cgi.com>
2020-02-25 18:44:12 +0100

    initial commit

--- Just_DW/dbo/Tables/TBL_ACCOUNT.sql
+++ Just_DW/dbo/Tables/TBL_ACCOUNT.sql
@@ -2,7 +2,7 @@
     [AccountId]       INT           NOT NULL,
     [ParentAccountID] INT           NULL,
     [AccountName]     VARCHAR (50)  NULL,
-    [Address1]        VARCHAR (100) NULL,
+    [Address1]        VARCHAR (120) NULL,
     [Address2]        VARCHAR (100) NULL,
     [Address3]        VARCHAR (100) NULL,
     [Postcode]        VARCHAR (10)  NULL,
